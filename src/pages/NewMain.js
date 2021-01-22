b2e2e3fa7a0add9bbf3e91a4919717597db98c3e
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2021-01-22 14:39:20 +0900

    로지 통계 추가 공지 업데이트

--- src/pages/NewMain.js
+++ src/pages/NewMain.js
@@ -40,9 +40,11 @@ class Main extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="mainpage_notice">
-                    <span className="mainpage_notice_span">{intl.formatMessage({id: '공지사항1'})}</span>
-                </div>
+                <Link to={"/Detail?gameMode=1&character=21&bestWeapon=9"}>
+                    <div className="mainpage_notice">
+                        <span className="mainpage_notice_span">{intl.formatMessage({id: '공지사항1'})}</span>
+                    </div>
+                </Link>
                 <Search />
                 <div className="mainpage_size"></div>
                 <Footer isMain={true} />
